e3351039f54d41001660c62f4b66b4061fce8cde
wedyzza <ggwp.ezpezlmon@mail.ru>
2025-06-10 14:53:54 +0500

    fixing orders

--- app/routers/order.py
+++ app/routers/order.py
@@ -109,9 +109,7 @@ def create_order(payload: schemas.OrderCreateInput,db: Session = Depends(get_db)
         need_quantity = payload.qty
         final_price = 0
         opposite_order_direction = models.DirectionsOrders.BUY if payload.direction == models.DirectionsOrders.SELL else models.DirectionsOrders.SELL
-        print(opposite_order_direction)
         currency_orders_quantity = db.query(func.sum(models.Order.quantity - models.Order.filled_quantity)).filter(and_(models.Order.direction == opposite_order_direction, models.Order.filled == False, models.Order.deleted_at == None, models.Order.instrument_id == instrument.id)).first()[0]
-        print(currency_orders_quantity)
         if currency_orders_quantity is None or need_quantity > currency_orders_quantity:
             # raise HTTPException(status_code=400, detail='В данный момент в стакане нет столько валюты, сколько вы хотите обменять.')
                 return Response('thied one' ,status_code=423)
