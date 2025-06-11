db4607720513b984f710500d8c5b2abccf79cb66
wedyzza <ggwp.ezpezlmon@mail.ru>
2025-06-11 18:07:15 +0500

    trying to fix

--- app/functions.py
+++ app/functions.py
@@ -222,11 +222,19 @@ def making_a_deal(buy_order: models.Order, sell_order: models.Order, db: Session
 
     buy_quantity = buy_order.quantity - buy_order.filled
     sell_quantity = sell_order.quantity - sell_order.filled
-    if buy_quantity <= 0 or sell_quantity <= 0:
-        print(f"{buy_order.id} | {buy_order.quantity} - {buy_order.filled} = {buy_quantity} => buy quantity")
-        print(f"{sell_order.id} | {sell_order.quantity} - {sell_order.filled} = {sell_quantity} => sell quantity")
-        print()
+    # if buy_quantity <= 0 or sell_quantity <= 0:
+    #     print(f"{buy_order.id} | {buy_order.quantity} - {buy_order.filled} = {buy_quantity} => buy quantity")
+    #     print(f"{sell_order.id} | {sell_order.quantity} - {sell_order.filled} = {sell_quantity} => sell quantity")
+    #     print()
+    
     final_quantity = buy_quantity if buy_quantity <= sell_quantity else sell_quantity
+    
+    if buy_order.filled + final_quantity > buy_order.quantity or sell_order.filled + final_quantity > sell_order.quantity:
+        print("failed on filled > quantity")
+        print(f"{buy_order.id} | {buy_order.quantity} - {buy_order.filled} => buy quantity")
+        print(f"{sell_order.id} | {sell_order.quantity} - {sell_order.filled} => sell quantity")
+        print(f"final quantity - {final_quantity}")
+
     buy_instrument = (
         db.query(models.Instrument)
         .filter(
@@ -263,12 +271,6 @@ def making_a_deal(buy_order: models.Order, sell_order: models.Order, db: Session
         sell_order.status = models.StatusOrders.EXECUTED
     else:
         sell_order.status = models.StatusOrders.PARTIALLY_EXECUTED
-
-    if buy_order.filled > buy_order.quantity or sell_order.filled > sell_order.quantity:
-        print("failed on filled > quantity")
-        print(f"{buy_order.id} | {buy_order.quantity} - {buy_order.filled} => buy quantity")
-        print(f"{sell_order.id} | {sell_order.quantity} - {sell_order.filled} => sell quantity")
-        print(f"final quantity - {final_quantity}")
 
     db.commit()
     db.refresh(buy_order)
