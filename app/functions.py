2d6d6f59655a5b41eaf1e82743c91cd5d5fd4ea0
wedyzza <ggwp.ezpezlmon@mail.ru>
2025-06-14 08:40:49 +0500

    AAA

--- app/functions.py
+++ app/functions.py
@@ -220,8 +220,8 @@ def making_a_deal(buy_order: models.Order, sell_order: models.Order, db: Session
     buyer = db.query(models.User).filter(models.User.id == buy_order.user_id).first()
     seller = db.query(models.User).filter(models.User.id == sell_order.user_id).first()
 
-    buy_order = db.query(models.Order).with_for_update().get(buy_order.id)
-    sell_order = db.query(models.Order).with_for_update().get(sell_order.id)
+    # buy_order = db.query(models.Order).with_for_update().get(buy_order.id)
+    # sell_order = db.query(models.Order).with_for_update().get(sell_order.id)
     
     buy_quantity = buy_order.quantity - buy_order.filled
     sell_quantity = sell_order.quantity - sell_order.filled
@@ -286,17 +286,3 @@ def making_a_deal(buy_order: models.Order, sell_order: models.Order, db: Session
     db.refresh(buy_order)
     db.refresh(sell_order)
     db.refresh(transaction)
-
-
-
-    # except Exception as e:
-    #     try:
-    #         print("buyer balance:")
-    #         for b in buyer.balance:
-    #             print(f"{b.instrument_id} - {b.amount}")
-    #     except:
-    #         print('Ничего нет')
-    #     finally:
-    #         print(f"buyer_id - {buyer.id}")
-    #         print(f"seller_id - {seller.id}")
-    #         raise e
--- app/routers/order.py
+++ app/routers/order.py
@@ -202,7 +202,7 @@ def create_order(
             )
             .first()[0]
         )
-        print(f"we have - {currency_orders_quantity}, user need - {need_quantity}")
+        # print(f"we have - {currency_orders_quantity}, user need - {need_quantity}")
         if currency_orders_quantity is None or need_quantity > currency_orders_quantity:
             # raise HTTPException(status_code=400, detail='В данный момент в стакане нет столько валюты, сколько вы хотите обменять.')
             return Response("thied one", status_code=423)
